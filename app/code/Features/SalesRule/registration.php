753d09418a3a815d8e676b5a25b8e6ad68dec3e2
Artem Bychenko <artbychenko@gmail.com>
2024-02-04 17:52:46 +0200

    SalesRule - add Module

---
+++ app/code/Features/SalesRule/registration.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+/**
+ * @author Artem Bychenko artbychenko@gmail.com
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Features_SalesRule', __DIR__);
